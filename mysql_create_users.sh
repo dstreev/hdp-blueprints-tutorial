3bd57fd7db0f6ef6e807d31ded0cefdc66f1ba60
David W. Streever <david@streever.com>
2015-04-26 15:03:38 -0400

    drop script

--- mysql_create_users.sh
+++ mysql_create_users.sh
@@ -25,42 +25,35 @@ RANGER_AUDIT_USER=ranger_logger
 RANGER_AUDIT_USER_PASSWORD=ranger
 
 # ALL HOSTS SHOULD contain localhost
-AMBARI_HOSTS="localhost m1.hdp.local"
-HIVE_HOSTS="localhost m1.hdp.local m2.hdp.local"
-RANGER_HOSTS="localhost m1.hdp.local m2.hdp.local d1.hdp.local d2.hdp.local d3.hdp.local d4.hdp.local d5.hdp.local"
-OOZIE_HOSTS="localhost m2.hdp.local"
+AMBARI_HOSTS="% localhost m1.hdp.local"
+HIVE_HOSTS="% localhost m1.hdp.local m2.hdp.local"
+RANGER_HOSTS="% localhost m1.hdp.local m2.hdp.local d1.hdp.local d2.hdp.local d3.hdp.local d4.hdp.local d5.hdp.local"
+OOZIE_HOSTS="% localhost m2.hdp.local"
 
-#--user=user_name --password=your_password db_name
-if [ "${MYSQL_ROOT_PASSWORD}" == "" ]; then
-    CONN="-u ${MYSQL_ROOT_USER}"
-else
-    CONN="--user=${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASSWORD}"
-fi
-
-MYSQL=mysql
 
 if [ "$1" == "drop" ]; then
+    RUN_DROP_SCRIPT=/tmp/hdp_drop_mysql_users.sql
   for i in $(eval echo ${AMBARI_HOSTS}); do
     echo "Drop Ambari User from: ${i}"
-    eval "DROP USER ''${AMBARI_USER}''@''${i}'''"
+    echo "DROP USER '${AMBARI_USER}'@'${i}'" > $RUN_DROP_SCRIPT
   done
   for i in $(eval echo ${HIVE_HOSTS}); do
     echo "Drop Hive User from: ${i}"
-    eval "DROP USER ''${HIVE_USER}''@''${i}'''"
+    eval "DROP USER '${HIVE_USER}'@'${i}'" >> $RUN_DROP_SCRIPT
   done
   for i in $(eval echo ${OOZIE_HOSTS}); do
     echo "Drop Oozie User from: ${i}"
-    eval "DROP USER ''${OOZIE_USER}''@''${i}'''"
+    eval "DROP USER '${OOZIE_USER}'@'${i}'" >> $RUN_DROP_SCRIPT
   done
   for i in $(eval echo ${RANGER_HOSTS}); do
     echo "Drop Ranger users from: ${i}"
-    eval "DROP USER ''${RANGER_USER}''@''${i}'''"
-    eval "DROP USER ''${RANGER_AUDIT_USER}''@''${i}'''"
+    eval "DROP USER '${RANGER_USER}'@'${i}'" >> $RUN_DROP_SCRIPT
+    eval "DROP USER '${RANGER_AUDIT_USER}'@'${i}'" >> $RUN_DROP_SCRIPT
   done
   exit
 fi
 
-RUN_SCRIPT=/tmp/hdp_mysql.sql
+RUN_SCRIPT=/tmp/hdp_create_mysql_users.sql
 
 # Ambari DB
 echo "CREATE DATABASE IF NOT EXISTS ${AMBARI_DB};" > $RUN_SCRIPT
